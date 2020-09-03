21f1091a399876a8cc94519e048d8bc8332560c2
Hugo <hugo.bollon@gmail.com>
2020-09-03 19:12:55 +0200

    Processing algorithm for backtrack all lcs

--- lcs.go
+++ lcs.go
@@ -1,6 +1,9 @@
 package edlib
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // LCS takes two strings and compute their LCS(Longuest Subsequence Problem)
 func LCS(str1, str2 string) int {
@@ -70,15 +73,70 @@ func processLCSBacktrack(str1 string, str2 string, lcsMatrix [][]int, m, n int) 
 
 	if m == 0 || n == 0 {
 		return ""
-	}
-	if runeStr1[m-1] == runeStr2[n-1] {
-		return processLCSBacktrack(str1, str2, lcsMatrix, m-1, n-1) + string(str1[m-1])
-	}
-	if lcsMatrix[m][n-1] > lcsMatrix[m-1][n] {
-		return processLCSBacktrack(str1, str2, lcsMatrix, m, n-1)
+	} else {
+		if runeStr1[m-1] == runeStr2[n-1] {
+			return processLCSBacktrack(str1, str2, lcsMatrix, m-1, n-1) + string(str1[m-1])
+		}
+		if lcsMatrix[m][n-1] > lcsMatrix[m-1][n] {
+			return processLCSBacktrack(str1, str2, lcsMatrix, m, n-1)
+		}
 	}
 
 	return processLCSBacktrack(str1, str2, lcsMatrix, m-1, n)
+}
+
+// LCSBacktrackAll returns an array containing all common substrings between str1 and str2
+func LCSBacktrackAll(str1, str2 string) ([]string, error) {
+	runeStr1 := []rune(str1)
+	runeStr2 := []rune(str2)
+
+	if len(runeStr1) == 0 || len(runeStr2) == 0 {
+		return nil, errors.New("Can't process and backtrack any LCS with empty string")
+	} else if equal(runeStr1, runeStr2) {
+		return []string{str1}, nil
+	}
+
+	lcsMatrix := make([][]int, len(runeStr1)+1)
+	for i := 0; i <= len(runeStr1); i++ {
+		lcsMatrix[i] = make([]int, len(runeStr2)+1)
+		for j := 0; j <= len(runeStr2); j++ {
+			lcsMatrix[i][j] = 0
+		}
+	}
+
+	all := processLCSBacktrackAll(str1, str2, lcsProcess(runeStr1, runeStr2), len(str1), len(str2))
+	fmt.Println(all)
+	return []string{str1}, nil
+}
+
+func processLCSBacktrackAll(str1 string, str2 string, lcsMatrix [][]int, m, n int) map[string]struct{} {
+	// Convert strings to rune array to handle no-ASCII characters
+	runeStr1 := []rune(str1)
+	runeStr2 := []rune(str2)
+
+	// Map containing all commons substrings (Hash set builded from map)
+	substrings := make(map[string]struct{})
+
+	if m == 0 || n == 0 {
+		substrings[""] = struct{}{}
+	} else if runeStr1[m-1] == runeStr2[n-1] {
+		for key := range processLCSBacktrackAll(str1, str2, lcsMatrix, m-1, n-1) {
+			substrings[key+string(str1[m-1])] = struct{}{}
+		}
+	} else {
+		if lcsMatrix[m-1][n] >= lcsMatrix[m][n-1] {
+			for key := range processLCSBacktrackAll(str1, str2, lcsMatrix, m-1, n) {
+				substrings[key] = struct{}{}
+			}
+		}
+		if lcsMatrix[m][n-1] >= lcsMatrix[m-1][n] {
+			for key := range processLCSBacktrackAll(str1, str2, lcsMatrix, m, n-1) {
+				substrings[key] = struct{}{}
+			}
+		}
+	}
+
+	return substrings
 }
 
 // LCSEditDistance determines the edit distance between two strings using LCS function
