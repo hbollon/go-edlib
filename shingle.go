74511b4d3d5182531816995779751fc39aa5b763
Shriprajwal K <the_daemon_lord@Shriprajwals-MacBook-Air.local>
2021-11-12 14:55:55 +0530

    Lint shingle.go

--- shingle.go
+++ shingle.go
@@ -1,28 +1,34 @@
 package edlib
 
+// Shingle Find the k-gram of a string for a given k
+// Takes a string and an integer as parameters and return a map.
+// Returns an empty map if the string is empty or if k is 0
 func Shingle(s string, k int) map[string]int {
 	m := make(map[string]int)
 	if s != "" && k != 0 {
-		rune_of_s := []rune(s)
+		runeS := []rune(s)
 
-		for i := 0; i < len(rune_of_s)-k+1; i++ {
-			m[string(rune_of_s[i:i+k])]++
+		for i := 0; i < len(runeS)-k+1; i++ {
+			m[string(runeS[i:i+k])]++
 		}
 	}
 	return m
 }
 
+// ShingleSlidingWindow Find the k-gram of a string for a given k using a sliding window of size k
+// Takes a string and an integer as parameters and return a map.
+// Returns an empty map if the string is empty or if k is 0
 func ShingleSlidingWindow(s string, k int) map[string]int {
 	m := make(map[string]int)
 	if s != "" && k != 0 {
-		rune_of_s := []rune(s)
+		runeS := []rune(s)
 
-		window := rune_of_s[:k]
+		window := runeS[:k]
 		m[string(window)]++
 
-		for i := 1; i < len(rune_of_s)-k+1; i++ {
+		for i := 1; i < len(runeS)-k+1; i++ {
 			window = window[1:]
-			window = append(window, rune_of_s[i+k-1])
+			window = append(window, runeS[i+k-1])
 			m[string(window)]++
 		}
 	}
