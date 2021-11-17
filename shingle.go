e7151bf9d1335e2dc78309432cb51749fc9ec841
Shriprajwal K <the_daemon_lord@Shriprajwals-MacBook-Air.local>
2021-11-17 18:57:34 +0530

    Remove ShingleSlidingWindow

--- shingle.go
+++ shingle.go
@@ -14,23 +14,3 @@ func Shingle(s string, k int) map[string]int {
 	}
 	return m
 }
-
-// ShingleSlidingWindow Find the k-gram of a string for a given k using a sliding window of size k
-// Takes a string and an integer as parameters and return a map.
-// Returns an empty map if the string is empty or if k is 0
-func ShingleSlidingWindow(s string, k int) map[string]int {
-	m := make(map[string]int)
-	if s != "" && k != 0 {
-		runeS := []rune(s)
-
-		window := runeS[:k]
-		m[string(window)]++
-
-		for i := 1; i < len(runeS)-k+1; i++ {
-			window = window[1:]
-			window = append(window, runeS[i+k-1])
-			m[string(window)]++
-		}
-	}
-	return m
-}
